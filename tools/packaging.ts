e97ee12affe01154648454e387abd4c22339342b
njl07 <akanass@icloud.com>
2017-11-18 03:34:12 +0100

    feat(packaging): Clean packaging process

--- tools/packaging.ts
+++ tools/packaging.ts
@@ -9,8 +9,6 @@ import * as fs from 'fs-extra';
  */
 interface FileObject {
     name: string;
-    remove?: boolean;
-    externals?: boolean;
 }
 
 /**
@@ -41,11 +39,10 @@ class Packaging {
      * Function to copy one file
      *
      * @param file {string}
-     * @param externals {boolean}
      *
-     * @return {Observable<R>}
+     * @return {Observable<any>}
      */
-    private _copy(file: string, externals?: boolean): Observable<any> {
+    private _copy(file: string): Observable<any> {
         // copy package.json
         if (file.indexOf('package.json') !== -1) {
             return this._copyAndCleanupPackageJson(file);
@@ -53,16 +50,12 @@ class Packaging {
 
         // copy other files
         return <Observable<any>> Observable.create((observer) => {
-            let fileDest = file;
-            if (externals && file.indexOf('src/') !== -1) {
-                fileDest = `commonjs/${file.split('src/').pop()}`;
-            }
             fs.stat(`${this._srcPath}${file}`, (error, stats) => {
                 if (error) {
                     console.error('doesn\'t exist on copy =>', error.message);
                 }
                 if (stats && (stats.isFile() || stats.isDirectory())) {
-                    fs.copy(`${this._srcPath}${file}`, `${this._destPath}${fileDest}`, (err) => {
+                    fs.copy(`${this._srcPath}${file}`, `${this._destPath}${file}`, (err) => {
                         if (err) {
                             console.error('copy failed =>', err.message);
                         }
@@ -79,51 +72,11 @@ class Packaging {
     }
 
     /**
-     * Function to remove original file
-     *
-     * @param file {string}
-     * @param remove {boolean}
-     *
-     * @return {Observable<any>}
-     *
-     * @private
-     */
-    private _remove(file: string, remove?: boolean): Observable<any> {
-        // remove original files
-        return <Observable<any>> Observable.create((observer) => {
-            if (remove) {
-                fs.stat(`${this._srcPath}${file}`, (error, stats) => {
-                    if (error) {
-                        console.error('doesn\'t exist on remove =>', error.message);
-                    }
-
-                    if (stats && (stats.isFile() || stats.isDirectory())) {
-                        fs.remove(`${this._srcPath}${file}`, (err) => {
-                            if (err) {
-                                console.error('remove failed =>', err.message);
-                            }
-
-                            observer.next();
-                            observer.complete();
-                        });
-                    } else {
-                        observer.next();
-                        observer.complete();
-                    }
-                });
-            } else {
-                observer.next();
-                observer.complete();
-            }
-        });
-    }
-
-    /**
      * Function to cleanup package.json and _copy it to dist directory
      *
      * @param file {string}
      *
-     * @return {Observable<R>}
+     * @return {Observable<any>}
      *
      * @private
      */
@@ -171,8 +124,12 @@ class Packaging {
      * Function that _copy all files in dist directory
      */
     process() {
-        Observable.forkJoin(this._files.map((fileObject: FileObject) => this._copy(fileObject.name, fileObject.externals)
-            .flatMap(_ => this._remove(fileObject.name, fileObject.remove)))).subscribe(null, error => console.error(error));
+        Observable.forkJoin(
+            this._files.map(
+                (fileObject: FileObject) => this._copy(fileObject.name)
+            )
+        )
+            .subscribe(null, error => console.error(error));
     }
 }
 
