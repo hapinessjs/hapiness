989bfdfbc0394c321261cecddcdc3c35f25a9ea2
njl07 <akanass@icloud.com>
2017-11-20 11:37:22 +0100

    release(version): v1.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-export { Hapiness, HapinessModule, Inject, Injectable, Optional, Lib, InjectionToken,
-        CoreModuleWithProviders, OnRegister, OnStart, OnError, CoreModule, CoreDecorator, CoreProvide,
-        Extension, ExtensionWithConfig, makeDecorator, Type, OnModuleInstantiated, OnExtensionLoad } from './core';
+export {
+    Hapiness,
+    HapinessModule,
+    Inject,
+    Injectable,
+    Optional,
+    Lib,
+    InjectionToken,
+    CoreModuleWithProviders,
+    OnRegister,
+    OnStart,
+    OnError,
+    CoreModule,
+    CoreDecorator,
+    CoreProvide,
+    Extension,
+    ExtensionWithConfig,
+    makeDecorator,
+    Type,
+    OnModuleInstantiated,
+    OnExtensionLoad,
+    createDecorator,
+    extractMetadata
+} from './core';
 
-export { HttpServerExt, HapiConfig, Route, Lifecycle, HttpRequestInfo, OnEvent, OnGet, OnDelete, OnOptions,
-        OnPatch, OnPost, OnPut, OnPreAuth, OnPostAuth, OnPreHandler, OnPostHandler, OnPreResponse, Request,
-        ReplyWithContinue, ReplyNoContinue, Server, HttpServerService } from './extensions/http-server';
+export {
+    HttpServerExt, HapiConfig, Route, Lifecycle, HttpRequestInfo, OnEvent, OnGet, OnDelete, OnOptions,
+    OnPatch, OnPost, OnPut, OnPreAuth, OnPostAuth, OnPreHandler, OnPostHandler, OnPreResponse, Request,
+    ReplyWithContinue, ReplyNoContinue, Server, HttpServerService
+} from './extensions/http-server';
 
-export { SocketServerExt, Socket, SocketConfig, WebSocketServer, SocketServerService } from './extensions/socket-server';
+export {
+    SocketServerExt,
+    Socket,
+    SocketConfig,
+    WebSocketServer,
+    SocketServerService
+} from './extensions/socket-server';
